8b5b4106a9510c00497662c4d8f6ec5ad2088e42
Leonhard Goethe <67231111+Blender-Alien@users.noreply.github.com>
2020-06-21 16:49:21 +0200

    Update MainFrameWorkCP.py

--- MainFrameWorkCP.py
+++ MainFrameWorkCP.py
@@ -3,10 +3,13 @@
 def CommandInput(): # Multipler Aufruf des User Inputs
   pass
 
+def GeneralNavigation(): # Multipler Aufruf der Interpretation des User Inputs
+  pass
 
 # StartMenu ----------------------------------
 
 def FirstNavigation: # Erste Funktion von: StartMenu
   pass
   
-
+def StartMenu: # Erste Funktion des Programms
+  pass
