b06595be9be237c99f88e8afd10e39a84cb49acc
Leonhard Goethe <67231111+Blender-Alien@users.noreply.github.com>
2020-06-21 17:20:33 +0200

    Update MainFrameWorkCP.py

--- MainFrameWorkCP.py
+++ MainFrameWorkCP.py
@@ -1,15 +1,37 @@
+# CustomVariables ----------------------------
+
+Prefix = "/"
+
+
 # MainFrameWork ------------------------------
 
-def CommandInput(): # Multipler Aufruf des User Inputs
-  pass
+def CommandInput():  # Multipler Aufruf des User Inputs
+    pass
 
-def GeneralNavigation(): # Multipler Aufruf der Interpretation des User Inputs
-  pass
+
+def GeneralNavigation():  # Multipler Aufruf der Interpretation des User Inputs
+    pass
+
+
+def GeneralOutput(args):  # Multiple Ausgabe an Nutzer
+    # Ausgabe an Nutzer
+    print("[System.Output] " + args)
+
 
 # StartMenu ----------------------------------
 
-def FirstNavigation: # Erste Funktion von: StartMenu
-  pass
-  
-def StartMenu: # Erste Funktion des Programms
-  pass
+def FirstNavigation():  # Erste Funktion von: StartMenu
+    # Hilfestellung
+    hilfe = "Um eine Liste an Befehlen zu erhalten, nutzen Sie: [" + Prefix + "help] ."
+    GeneralOutput(hilfe)
+
+
+def StartMenu():  # Erste Funktion des Programms
+    # Erstausgabe an Nutzer
+    PrefixFest = "Standard Eingabeprefix: " + Prefix
+    GeneralOutput(PrefixFest)
+    # Abfrage nach Navigation
+    FirstNavigation()
+
+
+StartMenu()
