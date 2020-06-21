981aa4696966b7d4e82fd9c40245ce2578559681
Leonhard Goethe <67231111+Blender-Alien@users.noreply.github.com>
2020-06-21 17:34:31 +0200

    Update MainFrameWorkCP.py

--- MainFrameWorkCP.py
+++ MainFrameWorkCP.py
@@ -2,11 +2,34 @@
 
 Prefix = "/"
 
-
 # MainFrameWork ------------------------------
 
+def Navigation():  # Konsolenfunktion
+    #Variablen
+    userinput = "Error"
+    # Hilfestellung
+    hilfe = "Um eine Liste an Befehlen zu erhalten, nutzen Sie: [" + Prefix + "help] ."
+    GeneralOutput(hilfe)
+    # Eingabeschleife
+    while userinput == "Error":
+        userinput = CommandInput()
+
+
 def CommandInput():  # Multipler Aufruf des User Inputs
-    pass
+    #Variablen
+    returnvalue = None
+    #Benutzereingabe
+    userinput = str(input("> "))
+    #Checkauf richtiges Prefix
+    if userinput.startswith(Prefix):
+      pass
+    else: # Rückgabe mit Error
+      GeneralOutput("Ungültiges Prefix.")
+      returnvalue = "Error"
+      return returnvalue
+    #Rückgabe mit Erfolg
+    returnvalue = userinput
+    return returnvalue
 
 
 def GeneralNavigation():  # Multipler Aufruf der Interpretation des User Inputs
@@ -19,11 +42,6 @@ def GeneralOutput(args):  # Multiple Ausgabe an Nutzer
 
 
 # StartMenu ----------------------------------
-
-def FirstNavigation():  # Erste Funktion von: StartMenu
-    # Hilfestellung
-    hilfe = "Um eine Liste an Befehlen zu erhalten, nutzen Sie: [" + Prefix + "help] ."
-    GeneralOutput(hilfe)
 
 
 def StartMenu():  # Erste Funktion des Programms
