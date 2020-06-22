34d36972357af70d1a419eb4681978a5edf7d416
Leonhard Goethe <67231111+Blender-Alien@users.noreply.github.com>
2020-06-22 12:47:38 +0200

    Update MainFrameWorkCP.py

--- MainFrameWorkCP.py
+++ MainFrameWorkCP.py
@@ -5,60 +5,52 @@ Prefix = "/"
 
 # InBetweenOperator --------------------------
 
-def InBetweenNavigation():  # Konsolenfunktion
-    #Variablen
-    userinput = "Error"
-    recognized = "Error"
-    # Eingabeschleife
-    while userinput and recognized == "Error":
-        userinput = "Error"
-        userinput = CommandInput("Main/InBetween ")
-        #Kommandos
-        if userinput == "/help": #Help - Kommando
-            GeneralOutput("Mögliche Befehle:\n(/back)\n(/exit)")
-            recognized = "Erfolg"
-        elif userinput == "/back":
-            StartMenu()
-            recognized = "Erfolg"
-        elif userinput == "/exit":
-            exit()
-        else: # Befehl nicht erkannt
-            if userinput != "Error": # Check auf Prefix-Error
-                GeneralOutput("[" + userinput + "] ist kein gültiger Befehl! Nutze [/help].")
-            recognized = "Error"
-    InBetweenNavigation() # Wiederhohlung der Eingabe bei Print-Befehlen
 
 def NavigateToProceed(): # Zwischenschritt zum Kryptografischem Abteil
     #Variablen
     #Vorschläge
     GeneralOutput("Switched to Stage [InBetween]")
-    InBetweenNavigation()
+    MainNavigation("Main/InBetween ")
 
 
 # MainFrameWork ------------------------------
 
-def MainNavigation():  # Konsolenfunktion
+def MainNavigation(stage):  # Konsolenfunktion
     #Variablen
     userinput = "Error"
     recognized = "Error"
     # Eingabeschleife
     while userinput and recognized == "Error":
         userinput = "Error"
-        userinput = CommandInput("Main ")
+        userinput = CommandInput(stage)
         #Kommandos
-        if userinput == "/help": #Help - Kommando
-            GeneralOutput("Mögliche Befehle:\n(/help)\n(/setproceed)\n(/exit)")
-            recognized = "Erfolg"
-        elif userinput == "/setproceed": #Proceed - Kommando
-            NavigateToProceed()
-            recognized = "Erfolg"
-        elif userinput == "/exit":
-            exit()
-        else: # Befehl nicht erkannt
-            if userinput != "Error": # Check auf Prefix-Error
-                GeneralOutput("[" + userinput + "] ist kein gültiger Befehl! Nutze [/help].")
-            recognized = "Error"
-    MainNavigation() # Wiederhohlung der Eingabe bei Print-Befehlen
+        if stage == "Main ":
+            if userinput == f"{Prefix}help": #Help - Kommando
+                GeneralOutput(f"Mögliche Befehle:\n({Prefix}help)\n({Prefix}setproceedure)\n({Prefix}exit)")
+                recognized = "Erfolg"
+            elif userinput == f"{Prefix}setproceedure": #Proceed - Kommando
+                NavigateToProceed()
+                recognized = "Erfolg"
+            elif userinput == f"{Prefix}exit":
+                exit()
+            else: # Befehl nicht erkannt
+                if userinput != "Error": # Check auf Prefix-Error
+                    GeneralOutput("[" + userinput + f"] ist kein gültiger Befehl! Nutze [{Prefix}help].")
+                recognized = "Error"
+        if stage == "Main/InBetween ":
+            if userinput == f"{Prefix}help":  # Help - Kommando
+                GeneralOutput(f"Mögliche Befehle:\n({Prefix}back)\n({Prefix}exit)")
+                recognized = "Erfolg"
+            elif userinput == f"{Prefix}back":
+                StartMenu()
+                recognized = "Erfolg"
+            elif userinput == f"{Prefix}exit":
+                exit()
+            else:  # Befehl nicht erkannt
+                if userinput != "Error":  # Check auf Prefix-Error
+                    GeneralOutput("[" + userinput + f"] ist kein gültiger Befehl! Nutze [{Prefix}help].")
+                recognized = "Error"
+    MainNavigation(stage) # Wiederhohlung der Eingabe bei Print-Befehlen
 
 
 def CommandInput(stage):  # Multipler Aufruf des User Inputs
@@ -89,7 +81,7 @@ def StartMenu():  # Erste Funktion des Programms
     #Anmerkung der Stage
     GeneralOutput("Switched to Stage [Main]")
     # Abfrage nach Navigation
-    MainNavigation()
+    MainNavigation("Main ")
 
 # MainProgramm -------------------------------
 
